6b420e00c5a3f1dd4ab70acec02bcdb658f3c40c
beatricecaturano <74875067+beatricecaturano@users.noreply.github.com>
2021-01-05 22:53:10 +0100

    Add files via upload

---
+++ equazione di secondo grado.py
@@ -0,0 +1,24 @@
+equazione di secondo grado.py
+
+a=int(input("inserisci il valore di a"))
+b=int(input("inserisci il valore di b"))
+c=int(input("inserisci il valore di c"))
+ 
+if a!=0, b!=0, c!=0:
+ print("l'equazione è completa")
+ delta=int(b^2-4*a*c)
+ x1=int(-b-**delta/2*a)
+ x2=int(-b+**delta/2*a)
+ 
+if b=0:
+ print("l'equazione è pura")
+ x1=int(-**-c/a)
+ x2=int(+**-c/a)
+
+if c=0
+ print("l'equazione è spuria")
+ x1=int(0)
+ x2=int(-b/a)
+ 
+
+
