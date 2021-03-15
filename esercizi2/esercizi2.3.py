cc093aa78ac0a8a269db4f70b2d61708216c8343
beatricecaturano <74875067+beatricecaturano@users.noreply.github.com>
2021-03-15 07:21:48 +0100

    Add files via upload

--- esercizi2/esercizi2.3.py
+++ esercizi2/esercizi2.3.py
@@ -1,7 +1,9 @@
 
 
 import numpy as np
-riga = input('inserisci numero  row: ')
-m = np.array([[1,2,3],[4,5,6],[7,8,9]]) 
-print([row[1] for row in m])
+lista = np.array([[1,2,3],[4,5,6],[7,8,9]]) 
+print(lista)
+numero_colonna = int(input('colonna da visualizzare: '))
+print([row[numero_colonna] for row in lista])
+
 
