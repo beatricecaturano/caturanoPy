53532639eb2264be795412b98b9ff293a13f840b
beatricecaturano <74875067+beatricecaturano@users.noreply.github.com>
2021-03-21 19:22:26 +0100

    Add files via upload

---
+++ esercizio.media.py
@@ -0,0 +1,12 @@
+from numpy import mean
+mario = {"matematica": 6, "italiano": 7, "scienze": 7, "inglese": 4}
+media_mario = mean(mario)
+print(media_mario)
+
+giovanni = {"matematica": 4, "italiano": 6, "scienze": 5, "inglese": 7}
+media_giovanni = mean(giovanni)
+print(media_giovanni)
+
+paola = {"matematica": 9, "italiano": 6, "scienze": 8, "inglese": 8}
+media_paola = mean(paola)
+print(media_paola)
