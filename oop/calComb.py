8b5fb82c36b1b43b96d3eca61901de8b7fd61991
beatricecaturano <74875067+beatricecaturano@users.noreply.github.com>
2021-10-05 22:23:09 +0200

    Add files via upload

--- oop/calComb.py
+++ oop/calComb.py
@@ -1,1 +1,12 @@
 
+
+stringa = 'casa'
+listastringa = list(stringa)
+class calcComb:
+ def __init__(self, stringa):                                                                                                                            
+  self.listastringa = listastringa
+
+print(listastringa)
+
+
+
