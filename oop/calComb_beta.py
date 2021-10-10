d28161d1f526eb70fafc3910c6e7c4ea14bc9435
beatricecaturano <74875067+beatricecaturano@users.noreply.github.com>
2021-10-10 23:08:07 +0200

    Add files via upload

---
+++ oop/calComb_beta.py
@@ -0,0 +1,103 @@
+class calcComb():
+
+    def __init__(self, stringa):
+
+        self.__N = len(stringa)
+        self.__stringa = stringa
+        self.__listStringa = list(stringa)
+
+    def get_stringa(self):
+        return self.__stringa
+
+    def get_listStringa(self):
+        return self.__listStringa
+
+    def setStringa(self):
+       self.__stringa = 'casa'
+        
+         return self.__stringa
+    
+    def confUtil(self):
+        open words.italian.txt
+          if self.__stringa in words.italian.txt
+           return self.__stringa
+
+    # PERMUTAZIONI
+
+    def nPermutSenzaRip(self):
+        '''
+        restituire il numero di permutazioni SENZA ripetizione
+        '''
+        return 0
+
+    def nPermutConRip(self):
+        '''
+        restituire il numero di permutazioni CON ripetizione
+        '''
+        return 0
+
+    def permutSenzaRip(self):
+        '''
+        generare e restituire la lista di permutazioni SENZA ripetizione
+        '''
+        return 0
+
+    def permutConRip(self):
+        '''
+        generare e restituire la lista di permutazioni CON ripetizione
+        '''
+        return 0
+
+    # DISPOSIZIONI
+
+    def nDispSemplSenzaRip(self):
+        '''
+        restituire il numero di disposizioni semplici SENZA ripetizione
+        '''
+        return 0
+
+    def nDispSemplConRip(self):
+        '''
+        restituire il numero di disposizioni semplici CON ripetizione
+        '''
+        return 0
+
+    def dispSemplSenzaRip(self):
+        '''
+        generare e restituire la lista delle disposizioni semplici SENZA ripetizione
+        '''
+        return 0
+
+
+    def dispSemplConRip(self):
+        '''
+        generare e restituire la lista delle disposizioni semplici CON ripetizione
+        '''
+        return 0
+
+    # COMBINAZIONI
+
+    def nCombSemplSenzaRip(self):
+        '''
+        restituire il numero delle combinazioni SENZA ripetizione
+        '''
+        return 0
+
+    def nCombSemplConRip(self):
+        '''
+        restituire il numero delle combinazioni CON ripetizione
+        '''
+        return 0
+
+    def combSenzaRip(self):
+        '''
+        generare e restituire la lista delle combinazioni SENZA ripetizione
+        '''
+        return 0
+
+
+    def combConRip(self):
+        '''
+        generare e restituire la lista delle combinazioni CON ripetizione
+        '''
+        return 0
